3eb7c8d2efe4ee434c1f64a061c3e32c9a8c2903
lorenalugo <lugotlorenay@gmail.com>
2018-03-26 12:32:19 -0500

    arreglando url

--- proyectos/carrusel_citasModulo8/galeria.js
+++ proyectos/carrusel_citasModulo8/galeria.js
@@ -1,19 +1,19 @@
 var galeria = [
    { persona:"Buddha - बुद्धा",
      frase:"En la confrontación entre el arrollo y la roca, el arrollo siempre ganará, no por la fuerza, sino por la persistencia.",
-     foto:"http://www.imagexia.com/img/Cara-de-Buda.jpg"
+     foto:"//www.imagexia.com/img/Cara-de-Buda.jpg"
    },
    { persona:"Khalil Gibran - جبران خليل جبران بن ميخائل بن سعد",
      frase:"El silencio del envidioso está lleno de ruidos.",
-     foto:"http://www.hannaharendtcenter.org/wp-content/uploads/2015/05/111.jpg"
+     foto:"//www.hannaharendtcenter.org/wp-content/uploads/2015/05/111.jpg"
    },
    { persona:"Confucio - 孔子",
      frase: "Todo tiene belleza pero no todo el mundo la puede ver.",
-     foto:"http://3.bp.blogspot.com/-VlsuMSoivLU/VeMlD-LymUI/AAAAAAABKPU/Q8PYwsFbqxg/s1600/confucio.jpeg"
+     foto:"//3.bp.blogspot.com/-VlsuMSoivLU/VeMlD-LymUI/AAAAAAABKPU/Q8PYwsFbqxg/s1600/confucio.jpeg"
    },
    { persona:"Lev Nikoláievich Tolstói - Лев Николаевич Толстой",
      frase:"Mi felicidad consiste en que sé apreciar lo que tengo y no deseo con exceso lo que no tengo.",
-     foto:"http://malba.s3-website-sa-east-1.amazonaws.com/wp-content/uploads/2014/09/tolstoi-05.jpg"
+     foto:"//malba.s3-website-sa-east-1.amazonaws.com/wp-content/uploads/2014/09/tolstoi-05.jpg"
    },
    { persona:"Platón - Πλάτων",
      frase:"El más importante y principal negocio público es la buena educación de la juventud.",
